4ecaf60790f2afdd3d87f7ccfc9936ac84723226
“dolly500” <dolapoakamo01@gmail.com>
2024-05-29 02:17:54 +0100

    update

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaInstagramSquare, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -8,13 +9,13 @@ const Footer = () => {
           Jez <span>Tech</span>
         </h2>
         <div className='socials'>
-          <a href='' className='icon'>
+          <a href='https://instagram.com' className='icon'>
             <FaInstagramSquare />
           </a>
-          <a href='' className='icon'>
+          <a href='https://github.com' className='icon'>
             <FaGithub />
           </a>
-          <a href='' className='icon'>
+          <a href='https://linkedin.com' className='icon'>
             <FaLinkedin />
           </a>
         </div>
