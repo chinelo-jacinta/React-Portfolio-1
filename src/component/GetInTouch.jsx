f1dbebeced745775080f70f34652ae4e89a6f2af
“dolly500” <dolapoakamo01@gmail.com>
2024-05-29 20:37:02 +0100

    update

--- src/component/GetInTouch.jsx
+++ src/component/GetInTouch.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const GetInTouch = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
+      .then((result) => {
+        console.log(result.text);
+        alert('Message sent successfully!');
+      }, (error) => {
+        console.log(error.text);
+        alert('Failed to send message, please try again later.');
+      });
+  };
+
   return (
     <section id='contact'>
-      <form action='submit' className='form'>
+      <form ref={form} onSubmit={sendEmail} className='form'>
         <h3>Get in touch</h3>
         <div className='form-center'>
-          <input type='text' className='form-text' placeholder='first Name' />
-          <input type='text' className='form-text' placeholder='Last Name' />
-          <input
-            type='email'
-            className='form-text'
-            placeholder='Email Address'
-          />
-          <input type='text' className='form-text' placeholder='Phone No.' />
+          <input type='text' name='firstName' className='form-text' placeholder='First Name' required />
+          <input type='text' name='lastName' className='form-text' placeholder='Last Name' required />
+          <input type='email' name='email' className='form-text' placeholder='Email Address' required />
+          <input type='text' name='phone' className='form-text' placeholder='Phone No.' />
         </div>
-        <textarea name='' id=''></textarea>
-        <button type='submit'>submit</button>
+        <textarea name='message' className='form-text' placeholder='Your Message' required></textarea>
+        <button type='submit'>Submit</button>
       </form>
     </section>
   );
